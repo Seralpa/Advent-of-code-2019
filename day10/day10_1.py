d32a0e621794713cb82c000a8a522cc874507280
sergalo <sergio.alonso.pascual@alumnos.uva.es>
2019-12-10 16:35:25 +0100

    day 10 p1

--- day10/day10_1.py
+++ day10/day10_1.py
@@ -1,16 +1,26 @@
 from operator import sub
+from operator import add
 from fractions import Fraction
 def se_ven(p1,p2,asteroids):
-    dif=map(sub,p1,p2)
+    dif=tuple(map(sub,p2,p1))
     if dif[1]==0:
         #tratar este caso especial
-        pass
-    direction=Fraction(dif[0],dif[1])#ojo con los signos
-    
-    
+        d=(abs(dif[0])//dif[0],0)
+    elif dif[0]==0:
+        #otro caso especial...
+        d=(0,abs(dif[1])//dif[1])
+    else:
+        direction=Fraction(dif[0],dif[1])#ojo con los signos
+        d=(abs(direction.numerator),abs(direction.denominator))
+        d=(d[0]*abs(dif[0])//dif[0],d[1]*abs(dif[1])//dif[1])
+    while True:
+        p1=tuple(map(add,p1,d))
+        if p1==p2:
+            return True
+        elif asteroids[p1[0]][p1[1]]=="#":
+            return False
 
-
-with open("test.txt","r") as f:
+with open("input1.txt","r") as f:
     asteroids=[list(l.strip()) for l in f.readlines()]
 
 asteroid_locations=[]
@@ -19,8 +29,11 @@ for i in range(len(asteroids)):
         if asteroids[i][j]=='#':
             asteroid_locations.append((i,j))
 
-for loc in asteroid_locations:
+view_count=[0 for _ in asteroid_locations]
+for i,loc in enumerate(asteroid_locations):
     for ast in asteroid_locations:
         if loc==ast:
             continue
-        pass
+        if se_ven(loc,ast,asteroids):
+            view_count[i]+=1
+print(max(view_count))
