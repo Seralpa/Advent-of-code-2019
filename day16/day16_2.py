bb789b7c621c8ff4b6570e1ec0bbd4067c1efb50
sergalo <sergio.alonso.pascual@alumnos.uva.es>
2019-12-18 11:32:59 +0100

    day16

--- day16/day16_2.py
+++ day16/day16_2.py
@@ -1,21 +1,13 @@
 with open("input.txt","r") as f:
-    inp=[int(e) for e in f.readline().strip()]
-phase_in=[]
-for _ in range(10000):
-    phase_in.extend(inp)
+    phase_elems=[int(e) for e in f.readline().strip()]*10000
+offset=int("".join(list(map(str,phase_elems[:7]))))
 base_pattern=[0,1,0,-1]
 n_phase=100
+phase_elems=phase_elems[offset:]
 for n in range(n_phase):
-    print(n)
-    phase_out=[]
-    for out_elem in range(1,len(phase_in)+1):#va a empezar en 1 para evitar division por 0
-        aux=[]
-        if out_elem>len(phase_in)/2:
-            phase_out.append(abs(sum(phase_in[out_elem-1:]))%10)
-            continue
-        for i in range(out_elem-1,len(phase_in)):
-            aux.append(phase_in[i]*base_pattern[((i+1)//out_elem)%4])
-        phase_out.append(abs(sum(aux))%10)
-    phase_in=phase_out
-phase_out="".join(list(map(str,phase_out)))
-print("done")
+    print("phase: %d"%n)
+    quedan=len(phase_elems)
+    for out_elem in reversed(range(len(phase_elems)-1)):#-1 porque el ultimo elemento nunca cambia
+        #print(sum(phase_elems[out_elem:])%10)
+        phase_elems[out_elem]=(phase_elems[out_elem+1]+phase_elems[out_elem])%10
+print("".join(list(map(str,phase_elems[:8]))))
